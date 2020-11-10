ae473f7400b7658d52aa86194c08d273d6d4f8c1
Mustafa Tanis <mustafatanis97@gmail.com>
2020-11-10 15:52:40 -0500

    Working on it

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
 
         String str = "";
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a name");
+        String name = scan.next();
+
+        System.out.println(name);//
     }
 }
